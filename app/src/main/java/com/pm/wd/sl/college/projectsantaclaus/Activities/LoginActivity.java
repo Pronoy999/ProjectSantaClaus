13392112e21ead955284f1b280aab0e7a9621c73
Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>
2019-02-07 12:08:15 +0530

    Added the Login Activity and Digit Text.

--- app/src/main/java/com/pm/wd/sl/college/projectsantaclaus/Activities/LoginActivity.java
+++ app/src/main/java/com/pm/wd/sl/college/projectsantaclaus/Activities/LoginActivity.java
@@ -2,10 +2,14 @@ package com.pm.wd.sl.college.projectsantaclaus.Activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.pm.wd.sl.college.projectsantaclaus.R;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText _mobileNumber;
+    private Button _requestOTP, _confirmButton, _cancelButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
