51dc128d1d9267c9cdbc1eafb36445c104e5aa16
Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>
2019-03-05 10:18:38 +0530

    Removed ButterKnife

--- app/src/main/java/com/pm/wd/sl/college/projectsantaclaus/Activities/SignUpActivity.java
+++ app/src/main/java/com/pm/wd/sl/college/projectsantaclaus/Activities/SignUpActivity.java
@@ -1,45 +1,49 @@
 package com.pm.wd.sl.college.projectsantaclaus.Activities;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.pm.wd.sl.college.projectsantaclaus.R;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-
 public class SignUpActivity extends AppCompatActivity {
-    @BindView(R.id.fName)
+
     EditText fName;
-    @BindView(R.id.lName)
+
     EditText lName;
-    @BindView(R.id.contactEmail)
+
     EditText email;
-    @BindView(R.id.contactPhone)
+
     EditText phone;
-    @BindView(R.id.userName)
+
     EditText username;
-    @BindView(R.id.register_btn)
+
     Button register_btn;
-
-
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-        ButterKnife.bind(this);
+        initializeViews();
         register_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(getApplicationContext(),LoginActivity.class);
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
                 startActivity(intent);
             }
         });
+    }
+
+    private void initializeViews() {
+        fName = findViewById(R.id.fName);
+        lName = findViewById(R.id.lName);
+        email = findViewById(R.id.contactEmail);
+        phone = findViewById(R.id.contactPhone);
+        username = findViewById(R.id.userName);
+        register_btn = findViewById(R.id.register_btn);
     }
 }
