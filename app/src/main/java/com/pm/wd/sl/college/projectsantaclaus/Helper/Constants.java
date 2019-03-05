cbbbb4f50c2ef77cff3612690d95b50f52698a18
Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>
2019-03-05 11:57:16 +0530

    Implemented the Login Shared Preferences.

--- app/src/main/java/com/pm/wd/sl/college/projectsantaclaus/Helper/Constants.java
+++ app/src/main/java/com/pm/wd/sl/college/projectsantaclaus/Helper/Constants.java
@@ -4,13 +4,15 @@ public class Constants {
     public static final String API_URL = "http://ec2-13-127-208-122.ap-south-1.compute.amazonaws.com/api/v1/";
     public static String API_TOKEN = "v15xbg5qscb7uy4e";
     public static final char DEFAULT_OTP_CHAR = ' ';
+    public static final String SHARED_PREFERENCE_NAME = "fuckBH";
+    public static final String IS_LOGGED_IN="isLoggedIn";
 
     public static final int OTP_REQUEST_CODE = 1;
-    public static final int OT_VERFIY_CODE = 2;
+    public static final int OTP_VERFIY_CODE = 2;
 
     /**
      * JSON Constants.
      */
     public static final String JSON_PHONE_NUMBER = "phoneNumber";
-    public static final String JSON_RESPONSE="res";
+    public static final String JSON_RESPONSE = "res";
 }
