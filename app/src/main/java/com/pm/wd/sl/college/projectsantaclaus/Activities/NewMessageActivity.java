d60316b1153fcc82497dde6b6b87d8d5d2747700
Writwick <writzx@gmail.com>
2019-03-06 11:53:03 +0530

    added message activities.

---
+++ app/src/main/java/com/pm/wd/sl/college/projectsantaclaus/Activities/NewMessageActivity.java
@@ -0,0 +1,15 @@
+package com.pm.wd.sl.college.projectsantaclaus.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.pm.wd.sl.college.projectsantaclaus.R;
+
+public class NewMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_message);
+    }
+}
