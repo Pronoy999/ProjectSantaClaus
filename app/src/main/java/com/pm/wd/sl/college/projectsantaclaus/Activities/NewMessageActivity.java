12fe8449b23c45880bb033871ff1a9bbd395f975
Writwick <writzx@gmail.com>
2019-03-08 21:07:50 +0530

    added fetchmessages logic. added activity transitions. added file opener.

--- app/src/main/java/com/pm/wd/sl/college/projectsantaclaus/Activities/NewMessageActivity.java
+++ app/src/main/java/com/pm/wd/sl/college/projectsantaclaus/Activities/NewMessageActivity.java
@@ -1,15 +1,101 @@
 package com.pm.wd.sl.college.projectsantaclaus.Activities;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
+import com.pm.wd.sl.college.projectsantaclaus.Helper.FileUtils;
 import com.pm.wd.sl.college.projectsantaclaus.R;
 
+import java.io.FileInputStream;
+
 public class NewMessageActivity extends AppCompatActivity {
+    EditText _toReceiverEdit;
+    EditText _newMsgEditText;
+    ImageView _newMsgImageView;
+    ImageView _newMsgSendButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_message);
+
+        initializeViews();
+    }
+
+    private void initializeViews() {
+        _toReceiverEdit = findViewById(R.id.toReceiverEdit);
+        _newMsgEditText = findViewById(R.id.newMsgEditText);
+        _newMsgImageView = findViewById(R.id.newMsgImageView);
+        _newMsgSendButton = findViewById(R.id.newMsgSendButton);
+
+        String toRecv;
+        if (getIntent() != null && (toRecv = getIntent().getStringExtra("to_receiver_id")) != null) {
+            _toReceiverEdit.setText(toRecv); // const
+        }
+
+        _newMsgImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(FileUtils.newOpenImageIntent(false), 0xef54);
+            }
+        });
+
+        _newMsgSendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (_newMsgEditText.getText().toString().isEmpty() /*|| todo no image chosen*/) {
+                    // todo show error;
+                    return;
+                }
+
+                // todo send image and message
+                setResult(RESULT_OK);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (resultCode == Activity.RESULT_OK) {
+            if (data != null) {
+                Uri uri = data.getData();
+                if (uri != null) {
+                    try {
+                        ParcelFileDescriptor pfd = getContentResolver().openFileDescriptor(uri, "r");
+                        if (pfd != null) {
+                            FileInputStream fis = new FileInputStream(pfd.getFileDescriptor());
+                            int totalLen = fis.available();
+
+                            int bytesRead, len;
+
+                            byte[] buffer = new byte[8192];
+
+                            for (len = 0; (bytesRead = fis.read(buffer, 0, 8192)) != -1; len += bytesRead) {
+                                // todo process file in chunks
+                                // todo buffer and bytesRead;
+                                // todo preferably in background thread
+                                // todo save file with original file hash file name!!
+                            }
+                        }
+                    } catch (Exception ex) {
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        setResult(RESULT_CANCELED);
+        finish();
     }
 }
---
+++ app/src/main/java/com/pm/wd/sl/college/projectsantaclaus/Helper/FileUtils.java
@@ -0,0 +1,374 @@
+package com.pm.wd.sl.college.projectsantaclaus.Helper;
+
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.database.Cursor;
+import android.database.DatabaseUtils;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.webkit.MimeTypeMap;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.text.DecimalFormat;
+import java.util.Comparator;
+import java.util.List;
+
+public class FileUtils {
+    public static final String MIME_TYPE_AUDIO = "audio/*";
+    public static final String MIME_TYPE_TEXT = "text/*";
+    public static final String MIME_TYPE_IMAGE = "image/*";
+    public static final String MIME_TYPE_VIDEO = "video/*";
+    public static final String MIME_TYPE_APP = "application/*";
+    public static final String HIDDEN_PREFIX = ".";
+    static final String TAG = "FileUtils";
+    private static final boolean DEBUG = false; // Set to true to enable logging
+    public static Comparator<File> sComparator = new Comparator<File>() {
+        @Override
+        public int compare(File f1, File f2) {
+            return f1.getName().toLowerCase().compareTo(
+                    f2.getName().toLowerCase());
+        }
+    };
+    public static FileFilter sFileFilter = new FileFilter() {
+        @Override
+        public boolean accept(File file) {
+            final String fileName = file.getName();
+            return file.isFile() && !fileName.startsWith(HIDDEN_PREFIX);
+        }
+    };
+    public static FileFilter sDirFilter = new FileFilter() {
+        @Override
+        public boolean accept(File file) {
+            final String fileName = file.getName();
+            return file.isDirectory() && !fileName.startsWith(HIDDEN_PREFIX);
+        }
+    };
+
+    public static String getExtension(String uri) {
+        if (uri == null) {
+            return null;
+        }
+
+        int dot = uri.lastIndexOf(".");
+        if (dot >= 0) {
+            return uri.substring(dot);
+        } else {
+            return "";
+        }
+    }
+
+    public static boolean isLocal(String url) {
+        if (url != null && !url.startsWith("http://") && !url.startsWith("https://")) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isMediaUri(Uri uri) {
+        return "media".equalsIgnoreCase(uri.getAuthority());
+    }
+
+    public static Uri getUri(File file) {
+        if (file != null) {
+            return Uri.fromFile(file);
+        }
+        return null;
+    }
+
+    public static File getPathWithoutFilename(File file) {
+        if (file != null) {
+            if (file.isDirectory()) {
+                return file;
+            } else {
+                String filename = file.getName();
+                String filepath = file.getAbsolutePath();
+
+                String pathwithoutname = filepath.substring(0,
+                        filepath.length() - filename.length());
+                if (pathwithoutname.endsWith("/")) {
+                    pathwithoutname = pathwithoutname.substring(0, pathwithoutname.length() - 1);
+                }
+                return new File(pathwithoutname);
+            }
+        }
+        return null;
+    }
+
+    public static String getMimeType(File file) {
+
+        String extension = getExtension(file.getName());
+
+        if (extension.length() > 0)
+            return MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension.substring(1));
+
+        return "application/octet-stream";
+    }
+
+    public static String getMimeType(Context context, Uri uri) {
+        File file = new File(getPath(context, uri));
+        return getMimeType(file);
+    }
+
+    public static boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+
+    public static boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+    public static boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
+    }
+
+    public static boolean isGooglePhotosUri(Uri uri) {
+        return "com.google.android.apps.photos.content".equals(uri.getAuthority());
+    }
+
+    public static String getDataColumn(Context context, Uri uri, String selection,
+                                       String[] selectionArgs) {
+
+        Cursor cursor = null;
+        final String column = "_data";
+        final String[] projection = {
+                column
+        };
+
+        try {
+            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs, null);
+            if (cursor != null && cursor.moveToFirst()) {
+                if (DEBUG)
+                    DatabaseUtils.dumpCursor(cursor);
+
+                final int column_index = cursor.getColumnIndexOrThrow(column);
+                return cursor.getString(column_index);
+            }
+        } finally {
+            if (cursor != null)
+                cursor.close();
+        }
+        return null;
+    }
+
+    public static String getPath(final Context context, final Uri uri) {
+
+        if (DEBUG)
+            Log.d(TAG + " File -",
+                    "Authority: " + uri.getAuthority() +
+                            ", Fragment: " + uri.getFragment() +
+                            ", Port: " + uri.getPort() +
+                            ", Query: " + uri.getQuery() +
+                            ", Scheme: " + uri.getScheme() +
+                            ", Host: " + uri.getHost() +
+                            ", Segments: " + uri.getPathSegments().toString()
+            );
+
+        final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+
+        // DocumentProvider
+        if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+            // ExternalStorageProvider
+            if (isExternalStorageDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                if ("primary".equalsIgnoreCase(type)) {
+                    return Environment.getExternalStorageDirectory() + "/" + split[1];
+                }
+
+                // TODO handle non-primary volumes
+            }
+            // DownloadsProvider
+            else if (isDownloadsDocument(uri)) {
+
+                final String id = DocumentsContract.getDocumentId(uri);
+                final Uri contentUri = ContentUris.withAppendedId(
+                        Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+
+                return getDataColumn(context, contentUri, null, null);
+            }
+            // MediaProvider
+            else if (isMediaDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                Uri contentUri = null;
+                if ("image".equals(type)) {
+                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                } else if ("audio".equals(type)) {
+                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                }
+
+                final String selection = "_id=?";
+                final String[] selectionArgs = new String[]{
+                        split[1]
+                };
+
+                return getDataColumn(context, contentUri, selection, selectionArgs);
+            }
+        }
+        // MediaStore (and general)
+        else if ("content".equalsIgnoreCase(uri.getScheme())) {
+
+            // Return the remote address
+            if (isGooglePhotosUri(uri))
+                return uri.getLastPathSegment();
+
+            return getDataColumn(context, uri, null, null);
+        }
+        // File
+        else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+
+        return null;
+    }
+
+    public static File getFile(Context context, Uri uri) {
+        if (uri != null) {
+            String path = getPath(context, uri);
+            if (path != null && isLocal(path)) {
+                return new File(path);
+            }
+        }
+        return null;
+    }
+
+    public static String getReadableFileSize(int size) {
+        final int BYTES_IN_KILOBYTES = 1024;
+        final DecimalFormat dec = new DecimalFormat("##0.00");
+        final String KILOBYTES = " KB";
+        final String MEGABYTES = " MB";
+        final String GIGABYTES = " GB";
+        float fileSize = 0;
+        String suffix = KILOBYTES;
+
+        if (size > BYTES_IN_KILOBYTES) {
+            fileSize = size / BYTES_IN_KILOBYTES;
+            if (fileSize > BYTES_IN_KILOBYTES) {
+                fileSize = fileSize / BYTES_IN_KILOBYTES;
+                if (fileSize > BYTES_IN_KILOBYTES) {
+                    fileSize = fileSize / BYTES_IN_KILOBYTES;
+                    suffix = GIGABYTES;
+                } else {
+                    suffix = MEGABYTES;
+                }
+            }
+        }
+        return String.valueOf(dec.format(fileSize) + suffix);
+    }
+
+    public static BitmapDrawable getThumbnail(Context context, File file) {
+        return getThumbnail(context, getUri(file), getMimeType(file));
+    }
+
+    public static BitmapDrawable getThumbnail(Context context, Uri uri) {
+        return getThumbnail(context, uri, getMimeType(context, uri));
+    }
+
+    public static BitmapDrawable getThumbnail(Context context, Uri uri, String mimeType) {
+        if (DEBUG)
+            Log.d(TAG, "Attempting to get thumbnail");
+
+        if (!isMediaUri(uri)) {
+            Log.e(TAG, "You can only retrieve thumbnails for images and videos.");
+            return null;
+        }
+
+        Bitmap bm = null;
+        if (uri != null) {
+            final ContentResolver resolver = context.getContentResolver();
+            Cursor cursor = null;
+            try {
+                cursor = resolver.query(uri, null, null, null, null);
+                if (cursor.moveToFirst()) {
+                    final int id = cursor.getInt(0);
+                    if (DEBUG)
+                        Log.d(TAG, "Got thumb ID: " + id);
+
+                    if (mimeType.contains("video")) {
+                        bm = MediaStore.Video.Thumbnails.getThumbnail(
+                                resolver,
+                                id,
+                                MediaStore.Video.Thumbnails.MINI_KIND,
+                                null);
+                    } else if (mimeType.contains(FileUtils.MIME_TYPE_IMAGE)) {
+                        bm = MediaStore.Images.Thumbnails.getThumbnail(
+                                resolver,
+                                id,
+                                MediaStore.Images.Thumbnails.MINI_KIND,
+                                null);
+                    }
+                }
+            } catch (Exception e) {
+                if (DEBUG)
+                    Log.e(TAG, "getThumbnail", e);
+            } finally {
+                if (cursor != null)
+                    cursor.close();
+            }
+        }
+        return new BitmapDrawable(context.getResources(), bm);
+    }
+
+    public static Drawable getIcon(Context context, Uri fileUri, String mimeType) {
+        PackageManager pm = context.getPackageManager();
+        final Intent innt = new Intent(Intent.ACTION_VIEW);
+        innt.setData(fileUri);
+        innt.setType(mimeType);
+
+        final List<ResolveInfo> matches = pm.queryIntentActivities(innt, 0);
+        for (ResolveInfo match : matches) {
+            return match.loadIcon(pm);
+        }
+
+        return null;
+    }
+
+    public static Intent createGetContentIntent() {
+
+        final Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+
+        intent.setType("*/*");
+
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        return intent;
+    }
+
+    public static Intent newOpenDocumentIntent(boolean allowMultiple) {
+        return new Intent(Intent.ACTION_OPEN_DOCUMENT)
+                .setType("*/*")
+                .addCategory(Intent.CATEGORY_OPENABLE)
+                .putExtra(Intent.EXTRA_LOCAL_ONLY, true)
+                .putExtra(Intent.EXTRA_ALLOW_MULTIPLE, allowMultiple)
+                .putExtra("android.content.extra.SHOW_ADVANCED", true);
+    }
+
+
+    public static Intent newCreateDocumentIntent(String fileName, String mime) {
+        return newOpenDocumentIntent(false)
+                .setType(mime)
+                .putExtra(Intent.EXTRA_TITLE, fileName)
+                .setAction(Intent.ACTION_CREATE_DOCUMENT);
+    }
+
+    public static Intent newOpenImageIntent(boolean allowMultiple) {
+        return newOpenDocumentIntent(allowMultiple).setType("image/*");
+    }
+}
