db64c491986a6b57f78f38fbca9a4117b318cdea
Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>
2019-05-21 16:49:18 +0530

    Updated the Constants.

--- app/src/main/java/com/pm/wd/sl/college/projectsantaclaus/Helper/Constants.java
+++ app/src/main/java/com/pm/wd/sl/college/projectsantaclaus/Helper/Constants.java
@@ -3,9 +3,8 @@ package com.pm.wd.sl.college.projectsantaclaus.Helper;
 import com.amazonaws.regions.Regions;
 
 public class Constants {
-    public static final String API_URL = "http://ec2-13-127-208-122.ap-south-1.compute.amazonaws.com/api/v6/";
-    //public static final String API_URL = "http://192.168.0.169:7069/";
-    public static String API_TOKEN = "v15xbg5qscb7uy4e";
+    public static final String API_URL = "EC2_OR_SERVER_URL_FOR_CUSTOM_API";
+     public static String API_TOKEN = "v15xbg5qscb7uy4e";
     public static final char DEFAULT_OTP_CHAR = ' ';
     public static final String SHARED_PREFERENCE_NAME = "fuckBH";
     public static final String IS_LOGGED_IN = "isLoggedIn";
@@ -14,7 +13,7 @@ public class Constants {
     public static final int OTP_VERFIY_CODE = 2;
     public static final int NEW_MESSAGE_ACTIVITY_CODE = 3;
     public static final int NEW_CONVO_MSG_CODE = 4;
-    public static final String BUCKET_URL = "https://s3.ap-south-1.amazonaws.com/santa-claus/%s";
+    public static final String BUCKET_URL = "URL_FOR_S3_BUCKET";
     /**
      * JSON Constants.
      */
@@ -40,8 +39,8 @@ public class Constants {
     /**
      * AWS Constants.
      */
-    public static final String AWS_BUCKET_NAME = "santa-claus";
-    public static final String AWS_COGNITO_POOL_ID = "ap-south-1:af502823-ceca-4eb0-bea6-62ca289aa19a";
+    public static final String AWS_BUCKET_NAME = "BUCKET_NAME";
+    public static final String AWS_COGNITO_POOL_ID = "COGNITO_POOL_ID";
     public static final String AWS_REGION = Regions.AP_SOUTH_1.getName();
     public static final String AWS_STATE_COMPLETED = "COMPLETED";
 }
